f3bb15c47eca40ce73d09232bba06e5e6b91939c
madhava <dabaremadhava@gmail.com>
2018-11-17 13:22:03 +0530

    neon status

--- node/switch.h
+++ node/switch.h
@@ -1,15 +1,22 @@
 #include "clickButton.h"
 #define SW 2 // digitla pin 2
-#define FENCE_OK HIGH
-
-//extern ClickButton sw(SW, LOW,HIGH);
+#define FENCE_OK LOW
+int neonLife = 0;
+bool neonStat = false;
 
 void setupSW() {
   pinMode(SW, INPUT_PULLUP);
 }
 
 bool isFenceOK() {
-  return !(digitalRead(SW) ^ FENCE_OK);
+  if(!(digitalRead(SW) ^ FENCE_OK)){
+    neonLife = NEON_LIFE;
+    neonStat = true;
+  }
+  if(!neonLife--){
+    neonStat = 0;
+  }
+  return neonStat;
 }
 
 
